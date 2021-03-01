b08fc60f2af6368b871ead645bdf1cadfff4f2fc
Jakub Gawecki <71930261+jmgawecki@users.noreply.github.com>
2021-03-01 14:39:49 +0000

    core data implemented

--- OnlineGroceryStore/Links.swift
+++ OnlineGroceryStore/Links.swift
@@ -19,3 +19,16 @@ import Foundation
  <a href='https://www.freepik.com/vectors/food'>Food vector created by pikisuperstar - www.freepik.com</a>
  <a href='https://www.freepik.com/vectors/girl'>Girl vector created by pikisuperstar - www.freepik.com</a>
  */
+
+
+#warning("Tasks")
+#warning("When signed in, apps pushes to HomeVC that does not inlcude the TabBar. How to make a transition to HomeVC and makes it as a start point (removing all the previous VC from the navigation stack)")
+#warning("Cell layouts")
+#warning("rename files")
+#warning("put image download functions into Network Manager")
+#warning("clear the cache once the VC will disappear")
+#warning("favorite button how to resolve the ui when the image changes for every fifth cell, not the only one that was clicked")
+#warning("create a model for last orders")
+#warning("create a model for the basket")
+#warning("Create payment authorisation")
+#warning("create a button action in homeVC that extends and offers quick action, like changing your data or loging out or deleting the account")
